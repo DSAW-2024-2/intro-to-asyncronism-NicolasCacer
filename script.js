f61d5945360a3ccb642b626896789deb57cd0486
Nicolas <njcaceres04@gmail.com>
2024-09-07 18:35:45 -0500

    Managed filters

--- script.js
+++ script.js
@@ -57,15 +57,17 @@ document.addEventListener('DOMContentLoaded', () => {
                     growthRate = data.results[i].name;
                     document.getElementById('pokemonGrowth').innerHTML = `${growthName}<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/></svg>`;
                     pokemonsCardsContainer.innerHTML = "";
+                    previousVisiblePokemons = 0;
                     fetchGrowthPokemonsList(data.results[i].name);
                     document.getElementById('growthAll').classList.remove('hidden');
+                    
                 })
             }
             document.getElementById('growthAll').addEventListener('click',()=>{
                 growthRate = 'All';
                 pokemonsCardsContainer.innerHTML = "";
                 previousVisiblePokemons = 0;
-                document.getElementById('pokemonGrowth').innerHTML = `${growthRate}<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/></svg>`;
+                document.getElementById('pokemonGrowth').innerHTML = `Growth Rate<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/></svg>`;
                 document.getElementById('growthAll').classList.add('hidden');
                 fetchPokemonsList();
             });
@@ -330,7 +332,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 scrollDown();
             } else{
                 fetchGrowthPokemonsList(growthRate);
-                
             }
         } else{
             alert('Choose a number between 1 and 20')
